1c0cf8b958f097b54dd4ac0a124b21a5ef5843a8
JakeRose03 <Jake.rose8@icloud.com>
2025-01-28 22:08:48 -0500

    initial Spring boot setup

---
+++ WrestlingTech/src/test/java/com/WrestlingTech/WrestlingTech/WrestlingTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WrestlingTech.WrestlingTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrestlingTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
